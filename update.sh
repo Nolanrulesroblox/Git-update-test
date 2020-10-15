ad2a3400d5fff045f05251674898b5942c71baa9
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2020-10-15 10:50:44 -0700

    v0.0.2
    added link

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt install git -y
 sudo apt update
 sudo apt upgrade -y
-git pull https://github.com/Nolanrulesroblox/SU.git --assume-unchanged Localization/tmp
+git pull https://github.com/Nolanrulesroblox/Git-update-test.git --assume-unchanged Localization/tmp
 # put your git link above
 clear
 echo "updated"
