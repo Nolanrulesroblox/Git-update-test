ce278bee4d6772599ce8a51e92e5469745a0a787
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2020-10-15 11:02:07 -0700

    Updated?

--- update.sh
+++ update.sh
@@ -1,12 +1,60 @@
 #!/bin/bash
 sudo
 clear
-echo "installing/updating git"
+echo "installing/updating git and git repo"
 sudo apt update
+clear
 sudo apt install git -y
+clear
 sudo apt update
+clear
 sudo apt upgrade -y
-git pull https://github.com/Nolanrulesroblox/Git-update-test.git --assume-unchanged Localization/tmp
+clear
+git pull https://github.com/Nolanrulesroblox/Git-update-test.git
+clear
+sudo rm readme.md
+echo "temp files cleared"
 # put your git link above
 clear
 echo "updated"
+sleep 3
+clear
+echo "running script"
+sleep 3
+echo "10 seconds before auto system-upgrades press CTRL + C to stop"
+sleep 1
+echo "#"
+sleep 1
+echo "##"
+sleep 1
+echo "###"
+sleep 1
+echo "####"
+sleep 1
+echo "#####"
+sleep 1
+echo "######"
+sleep 1
+echo "#######"
+sleep 1
+echo "########"
+sleep 1
+echo "#########"
+sleep 1
+echo "##########"
+sudo apt update
+clear
+sudo apt upgrade -y
+clear
+sudo apt autoremove
+clear
+sudo apt autoremove --purge
+clear
+sudo apt update 
+clear
+sudo apt-get install update -y
+clear
+sudo apt-get upgrade -y
+clear
+echo "Done"
+exit
